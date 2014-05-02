68bbc116a3365b6134fe54b356dd003a96a1e326
Jan Marek <mail@janmarek.net>
2014-05-02 14:32:37 +0200

    routing

--- app/Router/RouterFactory.php
+++ app/Router/RouterFactory.php
@@ -14,12 +14,52 @@ class RouterFactory
 {
 
 	/**
-	 * @return \Nette\Application\Routers\RouteList
+	 * @return RouteList
 	 */
 	public static function createRouter()
 	{
 		$router = new RouteList();
+
+		$router[] = new Route('user-settings', [
+			'module' => 'User',
+			'presenter' => 'User',
+			'action' => 'default',
+		]);
+
+		$router[] = new Route('user-logout', [
+			'module' => 'User',
+			'presenter' => 'Login',
+			'action' => 'logout',
+		]);
+
+		$router[] = new Route('user-<presenter>[/<action>][/<id [0-9]+>]', [
+			'module' => 'User',
+			'presenter' => NULL,
+			'action' => 'default',
+			'id' => NULL,
+		]);
+
+		$router[] = new Route('profile/<id [0-9]+>', [
+			'module' => 'Presentation',
+			'presenter' => 'Profile',
+			'action' => 'default',
+		]);
+
+		$router[] = new Route('create-presentation', [
+			'module' => 'Presentation',
+			'presenter' => 'List',
+			'action' => 'create',
+		]);
+
+		$router[] = new Route('presentation-<presenter>[/<action>][/<id [0-9]+>]', [
+			'module' => 'Presentation',
+			'presenter' => NULL,
+			'action' => 'default',
+			'id' => NULL,
+		]);
+
 		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+
 		return $router;
 	}
 }
