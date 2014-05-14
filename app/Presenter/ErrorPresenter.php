a25137d4b7cda0aeaad2d9a76cbc3ac20d9d4b91
Jan Marek <mail@janmarek.net>
2014-05-14 15:34:23 +0200

    log exceptions in ajax mode

--- app/Presenter/ErrorPresenter.php
+++ app/Presenter/ErrorPresenter.php
@@ -2,6 +2,7 @@
 
 namespace Presidos\Presenter;
 
+use Nette\Application\BadRequestException;
 use Nette\Diagnostics\Debugger;
 
 class ErrorPresenter extends BasePresenter
@@ -9,14 +10,17 @@ class ErrorPresenter extends BasePresenter
 
 	public function renderDefault($exception)
 	{
+		if (!($exception instanceof BadRequestException)) {
+			Debugger::log($exception, Debugger::ERROR);
+		}
+
 		if ($this->isAjax()) {
 			$this->payload->error = TRUE;
 			$this->terminate();
-		} elseif ($exception instanceof \Nette\Application\BadRequestException) {
+		} elseif ($exception instanceof BadRequestException) {
 			$this->setView('404');
 		} else {
 			$this->setView('500');
-			Debugger::log($exception, Debugger::ERROR);
 		}
 	}
 
